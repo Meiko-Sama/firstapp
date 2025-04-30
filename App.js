c66fd7abddca8e49072f556ad552d25c4cc47d35
Aluno <thayna.matos.carlos@gmail.com>
2025-04-30 14:55:26 -0300

    Mensagem Parabens

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-web";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.first}>PARABENSSSSSS</Text>
+      {/* ESTILIZAÇÃO POR GRUPO  */}
+
+      <Text style={{ color: "white", fontSize: 20 }}> * ❤❤🍰🍰🎂🎂 * </Text>
+      {/* ESTILIZAÇÃO POR LINHA  */}
+
+      <Text style={[styles.third, { color: "white" }]}>Stephany</Text>
+      {/* ESTILIZAÇÃO POR GRUPO E LINHA */}
+
+      <View style={[styles.container, styles.card]}></View>
+      {/* MULTIPLOS GRUPOS */}
     </View>
   );
 }
@@ -13,8 +23,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ff96f6",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  first: {
+    color: "white",
+    fontStyle: "italic",
+    textShadowColor: "white",
+    textShadowOffset: { width: 1, height: 2 },
+    textShadowRadius: 30,
+    fontSize: 40,
+  },
+  third: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
